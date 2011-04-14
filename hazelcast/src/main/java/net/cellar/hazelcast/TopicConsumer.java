476fb918eff111c482d34eb330d3e870f62ee1cf
iocanel <iocanel@apache.org>
2011-04-14 08:57:38 +0300

    CHG: Fixed Topic Consumer/Producer which did not properly respect switches.

--- hazelcast/src/main/java/net/cellar/hazelcast/TopicConsumer.java
+++ hazelcast/src/main/java/net/cellar/hazelcast/TopicConsumer.java
@@ -19,75 +19,76 @@ import net.cellar.core.event.EventConsumer;
 public class TopicConsumer<E extends Event> implements EventConsumer<E>, MessageListener<E> {
 
 
-	public static final String SWITCH_ID = "net.cellar.topic.consumer";
+    public static final String SWITCH_ID = "net.cellar.topic.consumer";
 
-	private final Switch eventSwitch = new BasicSwitch(SWITCH_ID);
+    private final Switch eventSwitch = new BasicSwitch(SWITCH_ID);
 
-	private ITopic topic;
-	private Dispatcher dispatcher;
-	private Node node;
+    private ITopic topic;
+    private Dispatcher dispatcher;
+    private Node node;
 
-	/**
-	 * Initialization method.
-	 */
-	public void init() {
-		if (topic != null) {
-			topic.addMessageListener(this);
-		}
-	}
+    /**
+     * Initialization method.
+     */
+    public void init() {
+        if (topic != null) {
+            topic.addMessageListener(this);
+        }
+    }
 
-	/**
-	 * Destruction method.
-	 */
-	public void destroy() {
-		if (topic != null) {
-			topic.removeMessageListener(this);
-		}
-	}
+    /**
+     * Destruction method.
+     */
+    public void destroy() {
+        if (topic != null) {
+            topic.removeMessageListener(this);
+        }
+    }
 
-	/**
-	 * Consumes an event form the topic.
-	 * @param event
-	 */
-	public void consume(E event) {
-		//Check if event has a specified destination.
-		if (event.getDestination() == null || event.getDestination().contains(node)) {
-			//Check is switch is on.
-			if (eventSwitch.getStatus().equals(SwitchStatus.ON) || !event.getBypassSwitches()) {
-				dispatcher.dispatch(event);
-			}
-		}
-	}
+    /**
+     * Consumes an event form the topic.
+     *
+     * @param event
+     */
+    public void consume(E event) {
+        //Check if event has a specified destination.
+        if (event.getDestination() == null || event.getDestination().contains(node)) {
+            //Check is switch is on.
+            if (eventSwitch.getStatus().equals(SwitchStatus.ON) || event.getBypassSwitches()) {
+                dispatcher.dispatch(event);
+            }
+        }
+    }
 
-	public void onMessage(E message) {
-		consume(message);
-	}
+    public void onMessage(E message) {
+        consume(message);
+    }
 
-	public ITopic<Event> getTopic() {
-		return topic;
-	}
+    public ITopic<Event> getTopic() {
+        return topic;
+    }
 
-	public void setTopic(ITopic<Event> topic) {
-		this.topic = topic;
-	}
+    public void setTopic(ITopic<Event> topic) {
+        this.topic = topic;
+    }
 
-	public Dispatcher getDispatcher() {
-		return dispatcher;
-	}
+    public Dispatcher getDispatcher() {
+        return dispatcher;
+    }
 
-	public void setDispatcher(Dispatcher dispatcher) {
-		this.dispatcher = dispatcher;
-	}
+    public void setDispatcher(Dispatcher dispatcher) {
+        this.dispatcher = dispatcher;
+    }
 
-	public Switch getSwitch() {
-		return eventSwitch;
-	}
+    public Switch getSwitch() {
+        return eventSwitch;
+    }
 
-	public Node getNode() {
-		return node;
-	}
+    public Node getNode() {
+        return node;
+    }
 
-	public void setNode(Node node) {
-		this.node = node;
-	}
+    public void setNode(Node node) {
+        this.node = node;
+    }
 }
--- hazelcast/src/main/java/net/cellar/hazelcast/TopicProducer.java
+++ hazelcast/src/main/java/net/cellar/hazelcast/TopicProducer.java
@@ -15,42 +15,43 @@ import net.cellar.core.event.EventProducer;
  *
  * @author iocanel
  */
-public class TopicProducer<E extends Event>  implements EventProducer<E> {
+public class TopicProducer<E extends Event> implements EventProducer<E> {
 
-	public static final String SWITCH_ID = "net.cellar.topic.producer";
+    public static final String SWITCH_ID = "net.cellar.topic.producer";
 
-	private final Switch eventSwitch = new BasicSwitch(SWITCH_ID);
-	private ITopic topic;
-	private Node node;
+    private final Switch eventSwitch = new BasicSwitch(SWITCH_ID);
+    private ITopic topic;
+    private Node node;
 
-	/**
-	 * Propagates an event into the distributed {@code ITopic}.
-	 * @param event
-	 */
-	public void produce(E event) {
-		if (eventSwitch.getStatus().equals(SwitchStatus.ON) || !event.getBypassSwitches() || event instanceof Result) {
-			event.setSource(node);
-			topic.publish(event);
-		}
-	}
+    /**
+     * Propagates an event into the distributed {@code ITopic}.
+     *
+     * @param event
+     */
+    public void produce(E event) {
+        if (eventSwitch.getStatus().equals(SwitchStatus.ON) || event.getBypassSwitches() || event instanceof Result) {
+            event.setSource(node);
+            topic.publish(event);
+        }
+    }
 
-	public Switch getSwitch() {
-		return eventSwitch;
-	}
+    public Switch getSwitch() {
+        return eventSwitch;
+    }
 
-	public ITopic<? extends Event> getTopic() {
-		return topic;
-	}
+    public ITopic<? extends Event> getTopic() {
+        return topic;
+    }
 
-	public void setTopic(ITopic<Event> topic) {
-		this.topic = topic;
-	}
+    public void setTopic(ITopic<Event> topic) {
+        this.topic = topic;
+    }
 
-	public Node getNode() {
-		return node;
-	}
+    public Node getNode() {
+        return node;
+    }
 
-	public void setNode(Node node) {
-		this.node = node;
-	}
+    public void setNode(Node node) {
+        this.node = node;
+    }
 }
